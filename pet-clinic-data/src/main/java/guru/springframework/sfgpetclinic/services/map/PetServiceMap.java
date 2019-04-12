019caffe5528832c4667975f8f74d1ae61050ff7
xav <xav_pav@yahoo.fr>
2019-04-12 14:46:27 +0200

    refactor to common crud interface. Closes #11

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+  @Override
+  public Set<Pet> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Pet finbyId(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Pet save(Pet pet) {
+    return super.save(pet.getId(), pet);
+  }
+
+  @Override
+  public void delete(Pet pet) {
+    super.delete(pet);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+
+  }
+}
