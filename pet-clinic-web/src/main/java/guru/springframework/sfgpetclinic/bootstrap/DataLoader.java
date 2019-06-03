265759c3872daec7f54039d28ebec2d02919896a
xav <xav_pav@yahoo.fr>
2019-06-03 17:46:10 +0200

    added Pet and contact to bootstrap. Closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -19,7 +22,8 @@ public class DataLoader implements CommandLineRunner {
   private final PetTypeService petTypeService;
 
   @Autowired
-  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+  public DataLoader(OwnerService ownerService, VetService vetService,
+                    PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
     this.petTypeService = petTypeService;
@@ -41,12 +45,33 @@ public class DataLoader implements CommandLineRunner {
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
     owner1.setLastName("Glenanne");
+    owner1.setAddress("123 Brickerel");
+    owner1.setCity("Miami");
+    owner1.setTelephone("1233213132145");
 
+    Pet mikesPet = new Pet();
+    mikesPet.setPetType(savedDogPetType);
+    mikesPet.setOwner(owner1);
+    mikesPet.setBirthDate(LocalDate.now());
+    mikesPet.setName("Rosco");
+    owner1.getPets().add(mikesPet);
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
     owner2.setFirstName("Fiona");
     owner2.setLastName("Glenanne");
+    owner2.setAddress("123 Brickerel");
+    owner2.setCity("Miami");
+    owner2.setTelephone("1233213132145");
+
+    Pet fionaPet = new Pet();
+    fionaPet.setPetType(savedCatPetType);
+    fionaPet.setOwner(owner2);
+    fionaPet.setBirthDate(LocalDate.now());
+    fionaPet.setName("Rosca");
+    owner2.getPets().add(fionaPet);
+    ownerService.save(owner2);
+
 
     ownerService.save(owner2);
 
