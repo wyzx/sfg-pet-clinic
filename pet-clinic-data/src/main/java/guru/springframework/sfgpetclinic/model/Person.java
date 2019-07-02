4a9c300eb883e42d14ea4aefbf1e3147c96ecbbd
xav <xav_pav@yahoo.fr>
2019-07-02 17:12:59 +0200

    wner JPA mapping. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
-import org.springframework.stereotype.Component;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
-@Component
+@MappedSuperclass
 public class Person extends BaseEntity {
+  @Column(name = "first_name")
   private String firstName;
+
+  @Column(name = "last_name")
   private String lastName;
 
   public String getFirstName() {
