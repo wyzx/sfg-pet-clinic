f3ad0a3f47b7c59ce805fce78a4f520f9bf7771b
xav <xav_pav@yahoo.fr>
2019-07-02 17:20:39 +0200

    vets JPA mapping. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,7 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+  @Column(name = "description")
   private String description;
+
 
   public String getDescription() {
     return description;
