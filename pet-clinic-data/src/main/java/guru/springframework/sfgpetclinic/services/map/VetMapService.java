671d4b53f91a7a5dbac88052041e371bbec79a86
xav <xav_pav@yahoo.fr>
2019-07-03 15:07:37 +0200

    refactor consistency service naming

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -10,11 +10,11 @@ import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
   private final SpecialtyService specialtyService;
 
-  public VetServiceMap(SpecialtyService specialtyService) {
+  public VetMapService(SpecialtyService specialtyService) {
     this.specialtyService = specialtyService;
   }
 
